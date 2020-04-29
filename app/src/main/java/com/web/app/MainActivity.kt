19e1f17d51dec1ddb99e98ef95c965e7b25930a4
Nguyen Tung <tungnv@runsystem.net>
2020-04-29 10:11:52 +0700

    base code

---
+++ app/src/main/java/com/web/app/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.web.app
+
+import android.net.http.SslError
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.SslErrorHandler
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val PAGE_URL = "https://ku777.info/"
+        const val MAX_PROGRESS = 100
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initWebView()
+        setWebClient()
+        loadUrl(PAGE_URL)
+    }
+
+    private fun initWebView() {
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override
+            fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                handler?.proceed()
+            }
+        }
+    }
+    private fun setWebClient() {
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(
+                    view: WebView,
+                    newProgress: Int
+            ) {
+                super.onProgressChanged(view, newProgress)
+                progressBar.progress = newProgress
+                if (newProgress < MAX_PROGRESS && progressBar.visibility == ProgressBar.GONE) {
+                    progressBar.visibility = ProgressBar.VISIBLE
+                }
+                if (newProgress == MAX_PROGRESS) {
+                    progressBar.visibility = ProgressBar.GONE
+                }
+            }
+        }
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check if the key event was the Back button and if there's history
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no web page history, exit the activity)
+        return super.onKeyDown(keyCode, event)
+    }
+    private fun loadUrl(pageUrl: String) {
+        webView.loadUrl(pageUrl)
+    }
+}
