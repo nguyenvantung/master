9642fd0ee36d2e2a187e60909317923190682106
nguyenvantung <nguyentung.fit.hut@gmail.com>
2020-04-30 10:42:50 +0700

    update

--- app/src/main/java/com/web/app/MainActivity.kt
+++ app/src/main/java/com/web/app/MainActivity.kt
@@ -3,12 +3,18 @@ package com.web.app
 import android.net.http.SslError
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.webkit.SslErrorHandler
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ProgressBar
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,9 +27,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initDataBase()
         initWebView()
         setWebClient()
-        loadUrl(PAGE_URL)
+    }
+
+    private fun initDataBase() {
+        val database = Firebase.database
+        val myRef = database.getReference("user")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val link = dataSnapshot.child("urlConfig").value as String ?: ""
+                if (link.isNotEmpty()) {
+                    loadUrl(link)
+                } else {
+                    loadUrl(PAGE_URL)
+                }
+
+                Log.d("MainActivity", "Value is: $link")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("MainActivity", "Failed to read value.", error.toException())
+                loadUrl(PAGE_URL)
+            }
+        })
     }
 
     private fun initWebView() {
@@ -38,11 +69,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun setWebClient() {
         webView.webChromeClient = object : WebChromeClient() {
             override fun onProgressChanged(
-                    view: WebView,
-                    newProgress: Int
+                view: WebView,
+                newProgress: Int
             ) {
                 super.onProgressChanged(view, newProgress)
                 progressBar.progress = newProgress
@@ -55,6 +87,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         // Check if the key event was the Back button and if there's history
         if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
@@ -64,6 +97,7 @@ class MainActivity : AppCompatActivity() {
         // If it wasn't the Back key or there's no web page history, exit the activity)
         return super.onKeyDown(keyCode, event)
     }
+
     private fun loadUrl(pageUrl: String) {
         webView.loadUrl(pageUrl)
     }
